873872f774fe5ca8218497360260a228526e8628
Гадеев Амир <amir.gadeev@gazprombank.ru>
2026-05-26 10:42:40 +0500

    fix: исправление ошибок линтера

--- src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
+++ src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
@@ -3,7 +3,6 @@ package ru.practicum.moviehub.http;
 import com.google.gson.reflect.TypeToken;
 import ru.practicum.moviehub.model.Movie;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class ListOfMoviesTypeToken extends TypeToken<List<Movie>> {
